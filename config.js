29a40571e2ff106d504c8c4aa7b6ccc8eebc5f9f
Sagar Munjal <munjal.sagar@gmail.com>
2016-09-06 22:57:46 +0530

    lets hope it works

--- config.js
+++ config.js
@@ -67,22 +67,53 @@ config = {
 
   // Development
   development: {
-    url: 'http://localhost:2368',
-    database: {
-      client: 'sqlite3',
-      connection: {
-        filename: path.join(__dirname, '/content/data/ghost-dev.db')
-      },
-      debug: false
-    },
-    server: {
-      host: '127.0.0.1',
-      port: '2368'
-    },
-    paths: {
-      contentPath: path.join(__dirname, '/content/')
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        // Change this to your Ghost blog's published URL.
+        url: 'http://localhost:2368',
+
+        // Example refferer policy
+        // Visit https://www.w3.org/TR/referrer-policy/ for instructions
+        // default 'origin-when-cross-origin',
+        // referrerPolicy: 'origin-when-cross-origin',
+
+        // Example mail config
+        // Visit http://support.ghost.org/mail for instructions
+        // ```
+        //  mail: {
+        //      transport: 'SMTP',
+        //      options: {
+        //          service: 'Mailgun',
+        //          auth: {
+        //              user: '', // mailgun username
+        //              pass: ''  // mailgun password
+        //          }
+        //      }
+        //  },
+        // ```
+
+        // #### Database
+        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        // #### Server
+        // Can be host & port (default), or socket
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        },
+        // #### Paths
+        // Specify where your content directory lives
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        }
     }
-  }
 
 };
 
